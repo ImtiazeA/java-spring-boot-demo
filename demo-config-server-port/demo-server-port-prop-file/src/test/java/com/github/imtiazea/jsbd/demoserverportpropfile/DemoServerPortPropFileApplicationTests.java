e83c4ff99c74d982ba6063e928cd5a5247f46b38
ImtiazeA <imtiaze_a@yahoo.com>
2024-01-15 10:07:44 +0600

    Completed Spring Boot Demo - Configure Server Port - Property File

---
+++ demo-config-server-port/demo-server-port-prop-file/src/test/java/com/github/imtiazea/jsbd/demoserverportpropfile/DemoServerPortPropFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.imtiazea.jsbd.demoserverportpropfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoServerPortPropFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
